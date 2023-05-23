bc2e11c57af952ba311c12f1c50428cf267ee0e6
kwangjaeyoo <yookj00@gmail.com>
2023-05-23 11:36:46 +0900

    reservations

---
+++ app/reservations/ReservationClient.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { toast } from 'react-hot-toast'
+import axios from 'axios'
+import { useCallback, useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { SafeReservations } from '@/app/types'
+import Heading from '@/app/components/Heading'
+import Container from '@/app/components/Container'
+import ListingCard from '@/app/components/listing/ListingCard'
+import { User } from '@prisma/client'
+
+interface ReservationsClientProps {
+  reservations: SafeReservations[] | Error
+  currentUser?: User | null
+}
+
+const ReservationClient: React.FC<ReservationsClientProps> = ({
+  reservations,
+  currentUser,
+}) => {
+  const router = useRouter()
+  const [deletingId, setDeletingId] = useState('')
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id)
+
+      axios
+        .delete(`/api/reservation/${id}`)
+        .then(() => {
+          toast.success('Reservation cancelled')
+          router.refresh()
+        })
+        .catch(() => {
+          toast.error('Something went wrong.')
+        })
+        .finally(() => {
+          setDeletingId('')
+        })
+    },
+    [router],
+  )
+
+  console.log(typeof reservations)
+  if (!(reservations instanceof Error)) console.log(reservations)
+
+  return (
+    <Container>
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+      <div
+        className="
+          mt-10
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          md:grid-cols-3 
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {!(reservations instanceof Error) &&
+          reservations.map((reservation: any) => (
+            <ListingCard
+              key={reservation.id}
+              data={reservation.listing}
+              reservation={reservation}
+              actionId={reservation.id}
+              onAction={onCancel}
+              disable={deletingId === reservation.id}
+              actionLabel="Cancel guest reservation"
+              currentUser={currentUser}
+            />
+          ))}
+      </div>
+    </Container>
+  )
+}
+
+export default ReservationClient
---
+++ app/reservations/page.tsx
@@ -0,0 +1,50 @@
+import EmptyState from '../components/EmptyState'
+import ClientOnly from '../components/ClientOnly'
+
+import getCurrentUser from '../actions/getCurrentUser'
+import getReservations from '../actions/getReservations'
+import ReservationClient from './ReservationClient'
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    )
+  }
+
+  const reservations = await getReservations({ userId: currentUser.id })
+
+  if (!(reservations instanceof Error)) {
+    if (reservations.length === 0) {
+      return (
+        <ClientOnly>
+          <EmptyState
+            title="No reservations found"
+            subtitle="Looks like you have no reservations on your properties."
+          />
+        </ClientOnly>
+      )
+    }
+  } else {
+    return (
+      <ClientOnly>
+        <EmptyState title="ERROR" subtitle={JSON.stringify(reservations)} />
+      </ClientOnly>
+    )
+  }
+
+  return (
+    <ClientOnly>
+      <ReservationClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  )
+}
+
+export default ReservationsPage
